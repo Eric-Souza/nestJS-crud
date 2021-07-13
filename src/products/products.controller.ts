112833e744e34f091fcafa7e2161fa7d564f8319
Eric Souza <eric.bh18souza@gmail.com>
2021-07-13 19:25:21 -0300

    Get CRUD operations completed

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 
 import { ProductsService } from './products.service'
 
@@ -7,18 +7,19 @@ export class ProductsController {
 	constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-	addProduct(
-    @Body('title') prodTitle: string, 
-    @Body('description') prodDesc: string, 
-    @Body('price') prodPrice: number
-	) {
-		const generatedId = this.productsService.insertProduct(
-			prodTitle,  
-			prodDesc, 
-			prodPrice
-		)
-
+	addProduct(@Body('title') prodTitle: string, @Body('description') prodDesc: string, @Body('price') prodPrice: number) {
+		const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice)
 		return { id: generatedId }
 	}
+
+  @Get()
+  getAllProducts() {
+  	return this.productsService.getProducts()
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+  	return this.productsService.getSingleProduct(prodId)
+  }
 }
  
